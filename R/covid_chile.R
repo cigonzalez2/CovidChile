67c3f4e2873b64acc199b81f197fa9eb8ad45b47
Camilo González <cigonzalez2@uc.cl>
2022-03-02 10:32:42 -0300

    Update covid_chile.R

--- R/covid_chile.R
+++ R/covid_chile.R
@@ -6,46 +6,44 @@
 #' covid_chile()
 covid_chile <- function(){
   test <- suppressWarnings(require(rio))
-  if(test ==T ){
-    ruta1 <- 'https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/'
-    ruta2 <- 'master/output/producto5/TotalesNacionales.csv'
-    ruta <- paste(ruta1, ruta2, sep = '')
-    data = import(ruta, format = 'csv', header = T)
-
-    # Tabla nuevos  -----------------------------------------------------------
+  if (test == T) {
     
+    ruta1 <- "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/"
+    ruta2 <- "master/output/producto5/TotalesNacionales.csv"
+    ruta <- paste(ruta1, ruta2, sep = "")
+    
+    data = import(ruta, format = "csv", header = T)
     n <- length(data)
-    ultimos <- data[7, c((n-6):n)]
-    print('Los contagiados de los 7 días anteriores son:')
+    ultimos <- data[7, c((n - 6):n)]
+    
+    print("Los contagiados de los 7 días anteriores son:")
     print(ultimos)
     
-    # Gráfico nuevos  ---------------------------------------------------------
-
-    suppressWarnings(casos_nuevos <- as.vector(na.omit(as.numeric(data[7,]))))
-    fecha <- as.Date(colnames(data)[-1])
+    suppressWarnings(casos_nuevos <- as.vector(na.omit(as.numeric(data[7, 
+    ]))))
     
+    fecha <- as.Date(colnames(data)[-1])
     n <- length(casos_nuevos)
     tiempo <- 1:n
-
-    plot(
-      tiempo,
-      casos_nuevos,
-      pch = 20,
-      col = 'skyblue3',
-      lwd = 2,
-      type = 'l',
-      xlab = 'Tiempo',
-      ylab = 'Casos Nuevos',
-      las = 1,
-      bty = 'n',
-      main = 'Casos Diarios Covid-19 '
-    )
-
-
+    
+    plot(tiempo, casos_nuevos, pch = 20, col = "skyblue3", 
+         lwd = 2, type = "l", xlab = "Tiempo", ylab = "Casos Nuevos", 
+         las = 1, bty = "n", main = "Casos Diarios Covid-19 ")
+    
     linea <- smooth.spline(tiempo, casos_nuevos, spar = 0.5)
-    lines(linea, col = 'orange', lwd = 2)
+    lines(linea, col = "orange", lwd = 2)
+    
+    print(
+      '________________________________________________________________________'
+    )
+    
+    print('Un resumen de los últimos 7 días:')
+    print(
+      summary(casos_nuevos[(length(casos_nuevos)-6): length(casos_nuevos)])
+    )
   }
-  else{print('Se necesita instalar la librería rio')}
-
+  else {
+    print("Se necesita instalar la librería rio")
+  }
 }
 
